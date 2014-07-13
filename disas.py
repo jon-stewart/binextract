da7ea0cde2ef4c605311251ffcaf630b4cef9f5c
jon-stewart <jon.linux@ssl-mail.com>
2014-07-13 17:03:13 +0100

    shellcode output, but with mistakes..

--- disas.py
+++ disas.py
@@ -2,6 +2,7 @@
 
 import sys
 import struct
+import operator
 
 options         = ["-h", "-s"]
 
@@ -14,6 +15,12 @@ elf_inst_arch   = {0x2:"SPARC", 0x3:"x86", 0x8:"MIPS", 0x14:"PowerPC",
                     0x28:"ARM", 0x2a:"SuperH", 0x32:"IA-64", 0x3e:"x86-64",
                     0xb7:"AArch64"}
 
+elf_fields      = ["e_ident_mag", "e_ident_class", "e_ident_data",
+                    "e_ident_version", "e_ident_osabi", "e_ident_abiversion",
+                    "e_type", "e_machine", "e_version", "e_entry", "e_phoff",
+                    "e_shoff", "e_flags", "e_ehsize", "e_phentsize", "e_phnum",
+                    "e_shentsize", "e_shnum", "e_shstrndx"]
+
 elf_names       = ["magic", "class", "endian", "orig elf", "system", "abi ver",
                     "type", "arch", "ver", "entry point", "prog header",
                     "section header", "flags", "header size",
@@ -21,6 +28,9 @@ elf_names       = ["magic", "class", "endian", "orig elf", "system", "abi ver",
                     "section header size", "number of section headers",
                     "section header str tbl idx"]
 
+sh_names        = ["sh_name", "sh_type", "sh_flags", "sh_addr", "sh_offset",
+                    "sh_size", "sh_link", "sh_info", "sh_addralign",
+                    "sh_entsize"]
 
 def help_text():
     print("Usage:\n\t%s <elf path>\n" % sys.argv[0])
@@ -34,10 +44,10 @@ def disas_elf(fname, opt):
 
     try:
         with open(fname, "rb") as fp:
-            lines = fp.readlines()
-            data = lines[0]
+            data = fp.read()
     except:
         print("[!] file %s not found\n" % fname)
+
 
     # TODO: e_entr, e_phoff, e_shoff are 8byte if 64bit file
     elf_hdr_fmt = "4sBBBBB7sHHIIIIIHHHHHH"
@@ -45,25 +55,52 @@ def disas_elf(fname, opt):
 
     # remove the unused e_ident[EI_PAD]
     del elf_hdr[6]
+
+    elf_dict = {}
+    for i,field in enumerate(elf_fields):
+        elf_dict[field] = [i, elf_names[i], elf_hdr[i]]
+
     # set class
-    elf_hdr[1] = elf_class[elf_hdr[1]]
+    elf_dict["e_ident_class"][2] = elf_class[elf_dict["e_ident_class"][2]]
     # set endian
-    elf_hdr[2] = elf_endian[elf_hdr[2]]
+    elf_dict["e_ident_data"][2] = elf_endian[elf_dict["e_ident_data"][2]]
     # set target system
-    elf_hdr[4] = elf_tgt_sys[elf_hdr[4]]
+    elf_dict["e_ident_osabi"][2] = elf_tgt_sys[elf_dict["e_ident_osabi"][2]]
     # set object type
-    elf_hdr[6] = elf_type[elf_hdr[6]]
+    elf_dict["e_type"][2] = elf_type[elf_dict["e_type"][2]]
     # set target instruction set arch
-    elf_hdr[7] = elf_inst_arch[elf_hdr[7]]
+    elf_dict["e_machine"][2] = elf_inst_arch[elf_dict["e_machine"][2]]
 
     if opt == "-h":
         print("\nDisas elf header\n")
-        for k,v in enumerate(elf_names):
-            print("\t%s: %s" % (v, elf_hdr[k]))
+        sorted_dict = sorted(elf_dict.items(), key=operator.itemgetter(1))
+        for entry in sorted_dict:
+            v = entry[1]
+            print("\t%s: %s" % (v[1], v[2]))
+
     elif opt == "-s":
-        pass
-#        (sh_name, sh_type, sh_flags, sh_addr, sh_offset, sh_size, sh_link,
-#            sh_info, sh_addralign, sh_entsize) = struct.unpack("IIIIIIIIII", data[offset:offset + ])
+        sh_off  = elf_dict["e_shoff"][2]
+        sh_size = elf_dict["e_shentsize"][2]
+        sh_fmt = "IIIIIIIIII"
+
+        for i in range(0, elf_hdr[17]):
+            sh_hdr = struct.unpack(sh_fmt, data[sh_off:sh_off + sh_size])
+            sh_off += sh_size
+
+            sh_dict = {}
+            for k,v in enumerate(sh_names):
+                sh_dict[v] = sh_hdr[k]
+
+            # if SHT_PROGBITS
+            if (sh_dict["sh_type"] == 1):
+                code_addr = sh_dict["sh_offset"]
+                code_size = sh_dict["sh_size"]
+                code = data[code_addr:code_addr + code_size]
+
+                f = open("output", "w")
+                f.write(str(code))
+                f.close()
+
 
 
 opt = [o for o in options if o in sys.argv]
