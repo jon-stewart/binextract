2abb413f0a93bbd3b1dd5d0cbe401996fde698ff
jon-stewart <jon.linux@ssl-mail.com>
2014-07-13 18:26:12 +0100

    Tidy up

--- disas.py
+++ disas.py
@@ -32,12 +32,7 @@ sh_names        = ["sh_name", "sh_type", "sh_flags", "sh_addr", "sh_offset",
                     "sh_size", "sh_link", "sh_info", "sh_addralign",
                     "sh_entsize"]
 
-def help_text():
-    print("Usage:\n\t%s <elf path>\n" % sys.argv[0])
-    print("\t-h\toutput header")
-    print("\t-s\toutput shellcode")
-
-def disas_elf(fname, opt):
+def read_binary(fname):
     '''
     '''
     data = None
@@ -48,7 +43,12 @@ def disas_elf(fname, opt):
     except:
         print("[!] file %s not found\n" % fname)
 
+    return data
 
+
+def disassemble_elf_header(data):
+    '''
+    '''
     # TODO: e_entr, e_phoff, e_shoff are 8byte if 64bit file
     elf_hdr_fmt = "4sBBBBB7sHHIIIIIHHHHHH"
     elf_hdr = list(struct.unpack(elf_hdr_fmt, data[0:0x34]))
@@ -71,6 +71,43 @@ def disas_elf(fname, opt):
     # set target instruction set arch
     elf_dict["e_machine"][2] = elf_inst_arch[elf_dict["e_machine"][2]]
 
+    return elf_dict
+
+
+def disassemble_elf_code(data, sh_off, sh_size, sh_num):
+    '''
+    '''
+    sh_fmt = "IIIIIIIIII"
+
+    for i in range(0, sh_num):
+        sh_hdr = struct.unpack(sh_fmt, data[sh_off:sh_off + sh_size])
+        sh_off += sh_size
+
+        sh_dict = {}
+        for k,v in enumerate(sh_names):
+            sh_dict[v] = sh_hdr[k]
+
+        # if SHT_PROGBITS
+        if (sh_dict["sh_type"] == 1):
+            code_addr = sh_dict["sh_offset"]
+            code_size = sh_dict["sh_size"]
+            code_chunk = data[code_addr:code_addr + code_size]
+
+            code_fmt = "%dB" % code_size
+            code = (struct.unpack(code_fmt, code_chunk))
+            for i in code:
+                print("%x" % i)
+
+
+def disassemble_elf(fname, opt):
+    '''
+    '''
+    data = read_binary(fname)
+    if (data is None):
+        return
+
+    elf_dict = disassemble_elf_header(data)
+
     if opt == "-h":
         print("\nDisas elf header\n")
         sorted_dict = sorted(elf_dict.items(), key=operator.itemgetter(1))
@@ -81,26 +118,14 @@ def disas_elf(fname, opt):
     elif opt == "-s":
         sh_off  = elf_dict["e_shoff"][2]
         sh_size = elf_dict["e_shentsize"][2]
-        sh_fmt = "IIIIIIIIII"
+        sh_num  =  elf_dict["e_shnum"][2]
+        disassemble_elf_code(data, sh_off, sh_size, sh_num)
 
-        for i in range(0, elf_hdr[17]):
-            sh_hdr = struct.unpack(sh_fmt, data[sh_off:sh_off + sh_size])
-            sh_off += sh_size
 
-            sh_dict = {}
-            for k,v in enumerate(sh_names):
-                sh_dict[v] = sh_hdr[k]
-
-            # if SHT_PROGBITS
-            if (sh_dict["sh_type"] == 1):
-                code_addr = sh_dict["sh_offset"]
-                code_size = sh_dict["sh_size"]
-                code_chunk = data[code_addr:code_addr + code_size]
-
-                code_fmt = "%dB" % code_size
-                code = (struct.unpack(code_fmt, code_chunk))
-                for i in code:
-                    print("%x" % i)
+def help_text():
+    print("Usage:\n\t%s <elf path>\n" % sys.argv[0])
+    print("\t-h\toutput header")
+    print("\t-s\toutput shellcode")
 
 
 opt = [o for o in options if o in sys.argv]
@@ -109,4 +134,4 @@ fname = [arg for arg in sys.argv if arg not in options and arg != sys.argv[0]]
 if (len(opt) > 1) or (len(fname) != 1):
     help_text()
 else:
-    disas_elf(fname[0], opt[0])
+    disassemble_elf(fname[0], opt[0])
